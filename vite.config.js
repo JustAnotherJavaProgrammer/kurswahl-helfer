fecdd49e4a0152c7b239f7bebe4ec4b5c475abb4
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2021-11-15 23:30:59 +0100

    removed the accessors: true build option
    This build option is no longer needed for any workarounds.
    
    Also added gpu.js to speed up the course assignment calculations

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import {Plugin as ViteFonts} from "vite-plugin-fonts";
+import { Plugin as ViteFonts } from "vite-plugin-fonts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({compilerOptions: {accessors: true}}),
-    ViteFonts({
-      google: {
-        families: ["Montserrat", "Merriweather Sans", "Material Icons Round"],
-      }
-    })],
+  plugins: [svelte(/*{compilerOptions: {accessors: true}}*/),
+  ViteFonts({
+    google: {
+      families: ["Montserrat", "Merriweather Sans", "Material Icons Round"],
+    }
+  })],
 })
